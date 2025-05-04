2c497621f0e58619895a9b5ffa880058384c7e0b
aoubak <aoubak01@gmail.com>
2025-05-04 23:11:56 +0530

    new file:   index.html 	new file:   main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+
+// calculator
+
+  const screen = document.getElementById("screen");
+  const buttons = document.querySelectorAll("[data-value]");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      const value = button.getAttribute("data-value");
+
+      if (value === "AC") {
+        screen.value = "";
+      } else if (value === "DEL") {
+        screen.value = screen.value.slice(0, -1);
+      } else if (value === "=") {
+        try {
+          screen.value = eval(screen.value);
+        } catch {
+          screen.value = "Error";
+        }
+      } else {
+        screen.value += value;
+      }
+    });
+  });
+
